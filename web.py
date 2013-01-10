1ca2321937ccc031cc9558ed2fc18fb39a041406
Renan Cakirerk <renan.cakirerk@gmail.com>
2013-01-10 23:20:02 +0200

    Add server ip and server port to settings

--- web.py
+++ web.py
@@ -1,5 +1,7 @@
 import shelve
 
+import settings
+
 from flask import Flask
 from flask import render_template
 
@@ -12,5 +14,5 @@ def show_twitter_stats():
     return render_template('index.html', storage=storage)
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    app.debug = settings.debug
+    app.run(host=settings.server_ip, port=settings.server_port)
